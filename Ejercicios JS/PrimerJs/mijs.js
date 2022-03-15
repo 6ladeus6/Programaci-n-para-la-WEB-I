270ca9896eb8fe31a3ae805f3ebbead1ddc90542
6ladeus6 <cladeus@cesde.net>
2022-03-14 21:15:49 -0500

    JS

---
+++ Ejercicios JS/PrimerJs/mijs.js
@@ -0,0 +1,14 @@
+/* Calcular el salario de un empleado que gana  2.500.000 descontando el 8%
+de salud y el 10% de pensión  */
+
+/* var salario = 2500000;
+var salud = salario * 8 / 100;
+var pension = salario * 10 /100;
+var sueldo_a_pagar = salario - salud - pension;
+alert("Su sueldo es de: " + salario +
+            " \n Descuento de salud es de: " + salud +
+            "\n Descuento de pension es de: " + pension +
+             "\nEl sueldo a pagar es: " + sueldo_a_pagar);
+var salario */
+var nombre = "Alejandro";
+document.write("<h1>Hola " + nombre + "<h1>");
---
+++ Ejercicios JS/TallerJS/Ejercicio1/areat.js
@@ -0,0 +1,88 @@
+/* 2.	Aplica la fórmula a = (b*h)/2 para calcular el área de un
+ triángulo donde sus dimensiones base y altura se deben pedir al usuario que las digite. */
+
+/*  var a;
+var b=prompt('Ingrese la base del trinagulo','');
+var h=prompt('Ingrese la altura del trinagulo:','');
+
+a = (b * h)/2;
+document.write('El area del triangulo es: ');
+document.write(a);
+ */
+/* 
+3.	Realiza las operaciones de suma, resta, multiplicación, división, y módulo pidiéndole solo 2 números al usuario. */
+
+/* var s,r,m,div,mod;
+ var a = prompt('Ingrese el numero a: ','');
+ var b = prompt('Ingrese el numero b: ','');
+
+s = parseInt (a) + parseInt (b);
+r =  parseInt (a)  - parseInt (b);
+m = parseInt (a)  * parseInt (b);
+div = parseInt (a)  / parseInt (b);
+mod = parseInt (a)  % parseInt (b);
+
+document.write('La suma de : ' + a + ' + ' + b + ' es : ');
+document.write(s + '<br>');
+
+document.write('La resta de : ' + a + ' - ' + b + ' es: ');
+document.write(r + '<br>');
+
+document.write('La multiplicacion de : ' + a + ' * ' + b + ' es: ');
+document.write(m + '<br>');
+
+document.write('La division de : ' + a + ' / ' + b + ' es: ');
+document.write(div + '<br>');
+
+document.write('El modulo de : ' + a + ' % ' + b + ' es: ');
+document.write(mod + '<br>'); */
+
+/* 4.	Calcule el sueldo mensual de un trabajador ingresando ingresando el número de horas trabajadas en el mes 
+y el valor tanto como de las horas normales de trabajo y las horas extras. */
+
+/* var sm;
+ var htpm = prompt('Ingrese las horas trabajadas al mes: ','');
+ var vhn = prompt('Ingrese el valor de horas normales: ','');
+ var vhe = prompt('Ingrese el valor de horas extras: ','');
+
+ sm = parseInt((htpm * vhn)+vhe);
+
+ document.writeln('el salario neto mensual es: ')
+ document.write(sm) */
+
+/*  5.	Digita el ingreso del precio de un producto y se debe calcular el IVA, mostrar cuanto es el IVA que se agrega, 
+ mostrar el precio del producto sin IVA y el total a pagar. */
+
+ 
+/*  var pro = prompt('Ingrese el valor del producto: ','');
+
+
+var psi = pro / 1.19;
+var iva =psi * 0.19;
+var total = parseInt(psi) + parseInt (iva);
+
+document.writeln('EL IVA es: ' + iva + '<br>');
+document.writeln('El producto sin IVA es: ' + psi + '<br>');
+document.writeln('El total a pagar es: ' + total + '<br>'); */
+
+
+/* 6.	Calcule el porcentaje de alumnos y alumnas de un salón de clase, digitando el total de alumnos hombres y mujeres. */
+
+
+/* var edad = prompt('Ingrese la edad: ')
+switch(true){
+    case edad < 18 : alert('Usted es menor');
+    break;
+    default: alert('Adulto mayor');
+    break;
+} */
+
+var  dia = prompt('Ingrese un numero: ');
+switch( parseInt (dia)){
+    case 1: alert('lunes');
+    break;
+    case 2: alert('Martes');
+    default: alert('EL numero ingresado no existe');
+    break;
+
+}
